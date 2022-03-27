b8113da49a82cfdd5221d3e3db985456d40c9a20
Hovkic <hovkic15@gmail.com>
2022-03-27 14:58:06 -0700

    rework logic tree for playRound

--- js/script.js
+++ js/script.js
@@ -1,41 +1,67 @@
 const moves = ["rock", "paper", "scissors"]
 
 
-//function to run game
-function game() {
-    playRound();
-}
 
 
 // function for one round of play
-function playRound() {
+function playRound(playerSelection, computerSelection){
 
-    const playerSelection = userPlay();
-    const computerSelection = computerPlay();
-    const product = outcome(playerSelection, computerSelection);
-    console.log(product);
 
-}
+    if (playerSelection === "rock") {
+        if (computerSelection === "rock") {
+            console.log("Tie game! Player and Computer both chose ROCK.");
+        }
+        else if (computerSelection === "scissors") {
+            console.log("Player wins! Player chose ROCK, Computer chose SCISSORS.");
+            playerScore++;
+        }
+        else {
+            console.log("Player loses. Player chose ROCK, Computer chose PAPER.");
+            computerScore++;
+        }
+    }
+    if (playerSelection === "paper") {
+        if (computerSelection === "paper") {
+            console.log("Tie game! Player and Computer both chose PAPER.");
+        } else if (computerSelection === "rock") {
+            console.log("Player wins! Player chose PAPER, Computer chose ROCK.");
+            playerScore++;
+        } else {
+            console.log("Player loses. Player chose PAPER, Computer chose SCISSORS.");
+            computerScore++;
+            }
+        }
+    if (playerSelection === "scissors")
+        if (computerSelection === "scissors") {
+            console.log("Tie game! Player and Computer both chose SCISSORS.");
+        }
+        else if (computerSelection === "paper") {
+            console.log("Player wins! Player chose SCISSORS, Computer chose PAPER.");
+            playerScore++;
+        }
+        else {
+            console.log("Player loses. Player chose SCISSORS, Computer chose ROCK.");
+            computerScore++;
+        }
+    }
 
-//outcomes
-function outcome(playerMove, computerMove) {
 
-    if (playerMove === computerMove) {
-        return "tie";
-    } else if (playerMove === "rock" && computerMove === "scissors") {
-        return "Player wins. Rock beats Scissors."
-    } else if (playerMove === "paper" && computerMove === "rock") {
-        return "Player wins. Paper beats Rock."
-    } else if (playerMove === "scissors" && computerMove === "paper") {
-        return "Player wins. Scissors beats Paper."
-    }  else if  (playerMove === "rock" && computerMove === "paper") {
-        return "Player loses. Paper beats Rock."
-    } else if (playerMove === "paper" && computerMove === "scissors") {
-        return "Player loses. Scissors beats Paper."
-    } else if (playerMove === "scissors" && computerMove === "rock") {
-        return "Player loses. Rock beats Scissors."
+//function to run game
+function game() {
+    for (let i = 0; i < 5; i++) {
+
+        let playerSelection = userPlay();
+        const computerSelection = computerPlay()
+        console.log(playRound(playerSelection, computerSelection));
+        console.log("Player Score:" + playerScore)
+        console.log("Computer Score:" + computerScore)
+    
     }
 }
+
+//score 
+let playerScore = parseInt(0);
+let computerScore = parseInt(0);
 
 
 // computer choice
@@ -58,9 +84,9 @@ function userPlay() {
         }
         input = input.toLowerCase(); 
         check = validateInput(input);
-}
-return(input);
-}
+    }
+    return(input);
+    }
 
 //function to validate input (make sure player is inputting one of the 3 options: rock, paper, or scissors
 function validateInput(choice) {
@@ -74,19 +100,5 @@ function validateInput(choice) {
 
 
 //run game
-for (let i = 0; i < 5; i++) {
-    game();
-}
-
-/*
-//for loop to run 5 rounds of play
-for (let i = 0; i < 5; i++) {
-    console.log(game())
-}
- */
-
-
-
-
-
+game();
 
